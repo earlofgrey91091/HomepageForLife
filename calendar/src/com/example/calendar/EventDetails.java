5792ba62284eeb1ac5399239fa6f5627ab7c2eb7
Philip Freidin <Philip@vpn3-145166.near.uiuc.edu>
2012-11-05 13:04:37 -0600

    hi

--- calendar/src/com/example/calendar/EventDetails.java
+++ calendar/src/com/example/calendar/EventDetails.java
@@ -6,28 +6,30 @@ import android.widget.TextView;
 
 public class EventDetails extends Activity {
 
-//	public CalendarEvent event;
-//	DbHandler db;
-//	int eventId = 1; //temporarily set to 1 until I figure out how to pass into this activity
+	public CalendarEvent event;
+	DbHandler db;
 	
 	public void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
-//        db = new DbHandler(this);
+        db = new DbHandler(this);
         setContentView(R.layout.activity_event_details);
-//		  event = db.getEvent(eventId);
+		event = db.getEvent(eventId);
 		
         TextView name = (TextView) findViewById(R.id.name);
-//        name.setText(event.getName());
-		name.setText("Birthday");
+        String formatName = new String("Name: ");
+//        formatName.concat(event.getName());
+		name.setText(formatName);
         
 		TextView date = (TextView) findViewById(R.id.date);
-//        date.setText(event.getDate());
-        date.setText("10/10/2013");
+		String formatDate = new String("Date: ");
+//        formatDate.concat(event.getDate());
+		date.setText(formatDate);
 
         TextView location = (TextView) findViewById(R.id.location);
-//        location.setText(event.getLocation());
-        location.setText("Ur moms house");
+        String formatLocation = new String("Location: ");
+//        formatLocation.concat(event.getLocation());
+		location.setText(formatLocation);
 		
 	}
 	
