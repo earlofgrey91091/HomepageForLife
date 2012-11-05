e4c8a23f0c2413d712e6fa5755e502dff769cdff
earlofgrey91091 <earlofgrey91091@gmail.com>
2012-11-05 13:02:49 -0600

    event details changes

--- calendar/src/com/example/calendar/EventDetails.java
+++ calendar/src/com/example/calendar/EventDetails.java
@@ -1,33 +1,29 @@
 package com.example.calendar;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class EventDetails extends Activity {
-
-//	public CalendarEvent event;
-//	DbHandler db;
-//	int eventId = 1; //temporarily set to 1 until I figure out how to pass into this activity
 	
 	public void onCreate(Bundle savedInstanceState) {
 		
-		super.onCreate(savedInstanceState);
-//        db = new DbHandler(this);
-        setContentView(R.layout.activity_event_details);
-//		  event = db.getEvent(eventId);
-		
-        TextView name = (TextView) findViewById(R.id.name);
-//        name.setText(event.getName());
-		name.setText("Birthday");
-        
-		TextView date = (TextView) findViewById(R.id.date);
-//        date.setText(event.getDate());
-        date.setText("10/10/2013");
-
-        TextView location = (TextView) findViewById(R.id.location);
-//        location.setText(event.getLocation());
-        location.setText("Ur moms house");
+	super.onCreate(savedInstanceState);
+	DbHandler db = new DbHandler(this);
+	setContentView(R.layout.activity_event_details);
+	Intent data = getIntent();
+	String theDate = (String) data.getSerializableExtra("Date");
+	CalendarEvent event = db.getEvent(theDate);
+	
+	TextView name = (TextView) findViewById(R.id.name);
+	name.setText(event.getName());
+	
+	TextView date = (TextView) findViewById(R.id.date);
+	date.setText(event.getDate());
+	
+	TextView location = (TextView) findViewById(R.id.location);
+	location.setText(event.getLocation());
 		
 	}
 	
